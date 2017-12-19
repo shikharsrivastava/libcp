f37be2a87ea3694bf9c9c768c8fc1372a7030474
shikharsrivastava <shikharsri1996@gmail.com>
2017-12-19 18:37:11 +0530

    Added wrapper make_functions for template deduction

--- ds/segmentTree.h
+++ ds/segmentTree.h
@@ -8,22 +8,32 @@ namespace ds {
  * Segment Tree class where
  * T - is the type of Node in segment Tree
  */
-template <typename T>
+template <typename T, typename F>
 class SegmentTree {
 
 	private:
 		int 			size;
+		int 			N;
 		std::vector<T>	tree;
+		int 			L;
+		int 			R;
+		F				mergeFunc;
 		
 	public:
 		// Constructors
-		SegmentTree();
-		SegmentTree(int n);
-
-		// build functions
-		template <typename I, typename F>
-		void build(I l, I r, F func, int i = 1);
+		SegmentTree(F func);
+		SegmentTree(F func, int n);
+		template <typename I>
+		SegmentTree(I l, I r, F func);
 		
+		// build functions
+		template <typename I>
+		void build(I l, I r, int i = 1);
+		
+		// query functions
+		T doActualQuery(int l, int r, int L, int R, int i = 1);
+		T query(int l, int r);
+
 		// Destructors
 		~SegmentTree();
 };
@@ -32,41 +42,106 @@ class SegmentTree {
 
 // Constructors
 
-template <typename T>
-SegmentTree<T>::SegmentTree()
-: tree(std::vector<T>(100000))
+template <typename T, typename F>
+SegmentTree<T, F>::SegmentTree(F func)
+: N(100001)
+, mergeFunc(func)
+, tree(std::vector<T>(4*N+1))
 {
+	L = 1;
+   	R = 100000;
+
 }
-template <typename T>
-SegmentTree<T>::SegmentTree(int n)
-: tree(std::vector<T>(n))
+template <typename T, typename F>
+SegmentTree<T, F>::SegmentTree(F func, int n)
+: N(n)
+, mergeFunc(func)
+, tree(std::vector<T>(4*N+1))
 {
+	L = 1;
+	R = N;
+
+}
+
+template <typename T, typename F>
+template <typename I>
+SegmentTree<T, F>::SegmentTree(I l, I r, F func)
+: N(r-l+1)
+, tree(std::vector<T>(4*N+1))
+, mergeFunc(func)
+{
+
+	L = 1;
+	R = std::distance(l, r) + 1;
+	build(l,r);
 }
 
 // Build functions
 // Assuming I is a random access iterator type
-template <typename T>
-template <typename I, typename F>
-void SegmentTree<T>::build(I l, I r, F func, int i) {
+template <typename T, typename F>
+template <typename I>
+void SegmentTree<T, F>::build(I l, I r, int i) {
+	
 	if (l == r)
 	{
 		tree[i] = *l;
 		return;
 	}
 
-	int m = l + (r-l)/2;
-	build(l, l+m, i*2);
-	build(l+m+1, r, i*2+1);
-	tree[i] = F(tree[2*1], tree[2*i+1]);
+	I m = std::next(l, std::distance(l,r)/2);
+	build(l, m, i*2);
+	build(m+1, r, i*2+1);
+	tree[i] = mergeFunc(tree[2*i], tree[2*i+1]);
 }
 
+// Query functions
+// Query wrapper
+template <typename T, typename F>
+T SegmentTree<T, F>::query(int l, int r)
+{
+	return doActualQuery(l, r, L, R, 1);
+}
+
+template <typename T, typename F>
+T SegmentTree<T, F>::doActualQuery(int l, int r, int L, int R, int i)
+{
+	if(l > R || r < L)
+	{
+		T ret(0);
+		return ret;
+	}
+	else if(L >= l && R <= r)
+		return tree[i];
+	else
+	{
+		int M = L + (R-L)/2;
+		T left = doActualQuery(l, r, L, M, i*2);
+		T right = doActualQuery(l, r, M+1, R, i*2+1);
+		return mergeFunc(left, right);
+	}
+}
 
 // Destructors
 
-template<typename T>
-SegmentTree<T>::~SegmentTree()
+template<typename T, typename F>
+SegmentTree<T, F>::~SegmentTree()
 {
 
+}
+
+
+/* wrapper make_ functions */
+
+template <typename T, typename I, typename F>
+SegmentTree<T, F> make_segmentTree(I l, I r, F mergeFunc)
+{
+	return SegmentTree<T, F>(l, r, mergeFunc);
+}
+
+template <typename T, typename F>
+SegmentTree<T, F> make_segmentTree(F mergeFunc, int n)
+{
+	return SegmentTree<T, F>(mergeFunc, n);
 }
 
 }
