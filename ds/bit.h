a6ab74c11cef921b6bee3bdc7e1a4c63138f6ca2
shikharsrivastava <shikharsri1996@gmail.com>
2017-12-20 20:21:41 +0530

    :

---
+++ ds/bit.h
@@ -0,0 +1,29 @@
+//19:55
+
+#ifndef LIBCP_BIT
+#define LIBCP_BIT
+
+#include<bits/stdc++.h>
+namespace libcp {
+namespace ds {
+
+template <typename T, typename F>
+class Bit
+{
+	public:
+	typedef int size_type;
+	typedef F	func_type;
+	// constructors
+	Bit();
+	Bit(size_type n);
+	template <typename I>
+	Bit(I s, I e, )
+
+};
+
+
+
+}
+}
+
+#endif
