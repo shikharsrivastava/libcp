459e055b5d23e263fb1b412af84e1fa520f56116
shikharsrivastava <shikharsri1996@gmail.com>
2017-12-20 20:22:09 +0530

    Added bit

--- ds/bit.h
+++ ds/bit.h
@@ -11,13 +11,24 @@ template <typename T, typename F>
 class Bit
 {
 	public:
+	// typedefs
 	typedef int size_type;
 	typedef F	func_type;
+
+	private:
+	// data members
+	
+
+	public:
 	// constructors
 	Bit();
 	Bit(size_type n);
 	template <typename I>
-	Bit(I s, I e, )
+	Bit(I s, I e, func_type addFunc);
+
+
+	// Bit functions
+	void
 
 };
 
